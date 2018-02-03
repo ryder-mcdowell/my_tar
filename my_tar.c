1a487fe5c3143b73056c030894e606f6d6077ced
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-02-02 16:28:01 -0800

    Write working with multiple files

--- my_tar.c
+++ my_tar.c
@@ -11,46 +11,48 @@ int main(int argc, char **argv) {
     fprintf(stderr, "usage: ./my_tar option [files...]\n      option = -c | -x\n");
   }
 
-  if ((argc == 3) && (strcmp(argv[1], "-c") == 0)) {
+  if ((argc == 2) && (strcmp(argv[1], "-c") == 0)) {
+    fprintf(stderr, "no files given\n");
+  }
+
+  if ((argc >= 3) && (strcmp(argv[1], "-c") == 0)) {
     FILE *fd;
     struct stat sb;
     int file_size;
     char *filename;
-    int c;
+    int c, i;
 
-    stat(argv[2], &sb);
+    //for each input file
+    for (i = 2; i < argc; i++) {
 
-    if (!S_ISREG(sb.st_mode)) {
-      fprintf(stderr, "WARNING: %s is not a regular file.. skipping.\n", argv[2]);
-    }
+      stat(argv[i], &sb);
 
-    filename = argv[2];
-    fd = fopen(argv[2], "r");
-
-    //write filename to stdout
-    fwrite(filename, sizeof(filename), 1, stdout);
-
-    //write file stat info to stdout
-    fwrite(&sb, sizeof(struct stat), 1, stdout);
-
-    //write file contents of file to stdout
-    char check;
-    while ((c = getc(fd)) != EOF) {               //Alok Singhal https://stackoverflow.com/questions/3463426/in-c-how-should-i-read-a-text-file-and-print-all-strings
-      check = putc(c, stdout);                    //Short and elegant!
-      if (check == EOF) {
-        fprintf(stderr, "Error\n");
-        perror("PUTC ERROR:");
+      if (!S_ISREG(sb.st_mode)) {
+        fprintf(stderr, "WARNING: %s is not a regular file.. skipping.\n", argv[i]);
       }
+
+      filename = argv[i];
+      fd = fopen(argv[i], "r");
+
+      //write filename to stdout
+      fwrite(filename, strlen(filename), 1, stdout);
+
+      //write file stat info to stdout
+      fwrite(&sb, sizeof(struct stat), 1, stdout);
+
+      //write file contents of file to stdout
+      char check;
+      while ((c = getc(fd)) != EOF) {                 //Alok Singhal https://stackoverflow.com/questions/3463426/in-c-how-should-i-read-a-text-file-and-print-all-strings
+        check = putc(c, stdout);                      //Short and elegant!
+        if (check == EOF) {
+          fprintf(stderr, "Error\n");
+          perror("PUTC ERROR:");
+        }
+      }
+      fclose(fd);
     }
-    fclose(fd);
-
-
-    printf("\n");
-
 
   }
-
-
 
   return 0;
 }
